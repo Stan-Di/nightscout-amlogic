c3b47cc5d7d1913574960f17f66a5bb13296d954
Stan-Di <147297339+Stan-Di@users.noreply.github.com>
2025-05-19 16:25:56 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+apt-get install -y gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg --dearmor
+echo "deb [ arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt update
+apt-get install -y mongodb-org=4.4.18 mongodb-org-server=4.4.18 mongodb-org-mongos=4.4.18 mongodb-org-tools=4.4.18 mongodb-org-shell=4.4.18 mongodb-org-database-tools-extra=4.4.18
+
+
+
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt-get install nodejs -y
+
+
+wget https://github.com/nightscout/cgm-remote-monitor/archive/refs/tags/15.0.3.tar.gz 
+tar xzvf 15.0.3.tar.gz
+mv cgm-remote-monitor-15.0.3 cgm
+cd cgm
+npm install
