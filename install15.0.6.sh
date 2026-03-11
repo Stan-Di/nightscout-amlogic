948dd4b5493d16821912186812383de79905cd23
Stan-Di <147297339+Stan-Di@users.noreply.github.com>
2026-03-11 12:11:53 +0300

    Create install15.0.6.sh
    Nightscout 15.0.6

---
+++ install15.0.6.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+apt-get install -y gnupg curl wget
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+apt update
+apt-get install -y mongodb-org=4.4.18 mongodb-org-server=4.4.18 mongodb-org-mongos=4.4.18 mongodb-org-tools=4.4.18 mongodb-org-shell=4.4.18 mongodb-org-database-tools-extra=4.4.18
+
+curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
+apt-get install nodejs -y
+
+wget https://github.com/nightscout/cgm-remote-monitor/archive/refs/tags/v15.0.6.tar.gz 
+tar xzvf v15.0.6.tar.gz
+mv cgm-remote-monitor-15.0.6 cgm
+cd cgm
+wget https://github.com/Stan-Di/nightscout-amlogic/raw/refs/heads/main/cgm.gz
+tar xzvf cgm.gz
+
+npm install
+
+systemctl start mongod
+systemctl enable mongod
